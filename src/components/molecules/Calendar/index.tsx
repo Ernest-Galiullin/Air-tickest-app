719276564412d82c0a09be3f1d9b3d03ceb71049
Ernest-Galiullin <91132298+Ernest-Galiullin@users.noreply.github.com>
2022-06-03 13:52:21 +0300

    fix bag

--- src/components/molecules/Calendar/index.tsx
+++ src/components/molecules/Calendar/index.tsx
@@ -2,22 +2,32 @@ import {useState} from 'react'
 import ReactCalendar from 'react-calendar'
 import './style.scss'
 
-function orderWeek(date: string) {
-  const week = date.slice(0, 4).slice(0, 2)
-  return `${date.slice(4, 10)}, ${week}`
-}
+const months = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь'
+]
+const weeks = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
 
 export default function Calendar(props: any) {
   const [value, setData] = useState(new Date())
 
   const handleChange = (data: typeof value) => {
-    const options: any = {
-      day: 'numeric',
-      month: 'long',
-      weekday: 'short'
-    }
-    const selectDate = orderWeek(data.toLocaleDateString('ru-RU', options))
-    props.onChange(selectDate)
+    const day = data.getDate()
+    const month = months[data.getMonth()].toLowerCase()
+    const week = weeks[data.getUTCDay()]
+    const ruFormatDate = `${day} ${month}, ${week}`
+
+    props.onChange(ruFormatDate)
     setData(data)
   }
 
